a3abc770ef02b946537f386da3215878e08584b9
4yerik <4yerik@gmail.com>
2020-03-05 22:01:56 -0500

    get launch by id

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
