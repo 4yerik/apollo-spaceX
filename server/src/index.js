5776b304c687ad8a55729e79887078deb17a899f
4yerik <4yerik@gmail.com>
2020-03-06 23:27:17 -0500

    resolvers

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,8 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+
 const LaunchAPI = require('./datasources/launch');
 const UserAPI = require('./datasources/user');
 
@@ -5,6 +10,7 @@ const store = createStore();
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
